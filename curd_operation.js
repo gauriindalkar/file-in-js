b383040edc1904a6399201daadb2ef14865982b0
gauriindalkar <84510274+gauriindalkar@users.noreply.github.com>
2021-12-27 11:41:21 +0530

    Add files via upload

---
+++ curd_operation.js
@@ -0,0 +1,28 @@
+// <<<<<<<<<<<<Curd
+
+const fs = require('fs')
+
+// fs.mkdirSync("file",(err)=>{
+//     console.log("folder created")
+// });
+
+// <<<<<<<<<<<<Create a file
+// fs.writeFileSync('file/gauri.txt',"my name is gauri vilas indalkar.");
+
+// <<<<<<<<<<<<Append a file
+// fs.appendFileSync('file/gauri.txt'," I am a coder.");
+
+// <<<<<<<<<<<<read a file
+// if i want to read file and i put in code only file name that time output 
+// is come in buffer but we will using utf8 for the read the file.
+// const data = fs.readFileSync('file/gauri.txt',"utf8");
+// console.log(data)
+
+// <<<<<<<<<<<<Rename file name
+// fs.renameSync('file/gauri.txt','file/gauri_bio.txt',);
+
+// <<<<<<<<<<<<unlinesync a file
+// fs.unlinkSync("gauri_bio.txt");
+
+// <<<<<<<<<<<<delete file 
+// fs.rmdirSync("file");
